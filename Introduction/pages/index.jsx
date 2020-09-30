339841bedcbc972cbadb096bd6f34c03048bd98d
Vincent <Vincent.bouton.dev@gmail.com>
2020-10-01 01:43:03 +0200

    Page navigation with the Link component

--- Introduction/pages/index.jsx
+++ Introduction/pages/index.jsx
@@ -1,11 +1,17 @@
 //DATE : 10/1/20
 
 import React from "react";
+import Link from "next/link";
 
 const index = () => {
   return (
     <div className={"index"}>
-      index page
+      <h1>Index page</h1>
+      <Link href={"/notes"}>
+        <a>
+          note
+        </a>
+      </Link>
     </div>
   )
 }
--- Introduction/pages/notes/index.jsx
+++ Introduction/pages/notes/index.jsx
@@ -1,12 +1,26 @@
 //DATE : 10/1/20
 
-import React, {useState} from "react";
+import React from "react";
+import Link from "next/link";
 
-const index = () => {
+const Notes = () => {
+  const notes = new Array(15).fill(1).map((e, i) => ({id: i, title: `Note: ${i}`}))
+
   return (
-    <div className={"index"}>
-      notes
+    <div>
+      <h1>Notes</h1>
+
+      {notes.map(note => (
+        <div key={note.id}>
+          <Link href="/notes/[id]" as={`/notes/${note.id}`}>
+            <a>
+              <strong>{note.title}</strong>
+            </a>
+          </Link>
+        </div>
+      ))}
     </div>
   )
 }
-export default index;
+
+export default Notes
