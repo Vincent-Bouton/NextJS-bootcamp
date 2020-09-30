ef61a198881590e04ac1288a07af7fac36693b3d
Vincent <Vincent.bouton.dev@gmail.com>
2020-10-01 01:15:45 +0200

    Dynamic route parameters

--- Introduction/pages/notes/[id].jsx
+++ Introduction/pages/notes/[id].jsx
@@ -1,12 +1,15 @@
 //DATE : 10/1/20
 
 import React from "react";
+import {useRouter} from "next/router";
 
 const id = () => {
+  const router =  useRouter()
+  const {id} = router.query
   return (
-    <div className={"[id]"}>
-      note 1
-    </div>
+    <h1 className={"id"}>
+      note {id}
+    </h1>
   )
 }
 export default id;
