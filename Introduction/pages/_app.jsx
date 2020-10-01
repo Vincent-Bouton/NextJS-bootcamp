9c47bf8063284d0c6ed394e6cb4e398e76316eb5
Vincent <Vincent.bouton.dev@gmail.com>
2020-10-01 02:42:01 +0200

    Theme UI

--- Introduction/pages/_app.jsx
+++ Introduction/pages/_app.jsx
@@ -1,7 +1,13 @@
 //DATE : 10/1/20
 
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { ThemeProvider } from "theme-ui";
+import theme from "../theme";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
