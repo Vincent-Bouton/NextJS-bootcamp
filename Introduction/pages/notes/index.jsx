2655b06aaa1aac261801b728a5ad26b88242b8b7
Vincent <Vincent.bouton.dev@gmail.com>
2020-10-03 05:26:45 +0200

    GetServerSideProps

--- Introduction/pages/notes/index.jsx
+++ Introduction/pages/notes/index.jsx
@@ -4,11 +4,7 @@
 import { jsx } from "theme-ui";
 import Link from "next/link";
 
-const Notes = () => {
-  const notes = new Array(15)
-    .fill(1)
-    .map((e, i) => ({ id: i, title: `Note: ${i}` }));
-
+const Notes = ({ notes }) => {
   return (
     <div sx={{ variant: "containers.page" }}>
       <h1>My Notes</h1>
@@ -22,8 +18,8 @@ const Notes = () => {
         }}
       >
         {notes.map((note) => (
-          <div sx={{ width: "33%", p: 2 }}>
-            <Link key={note.id} href="/notes/[id]" as={`/notes/${note.id}`}>
+          <div key={note.id} sx={{ width: "33%", p: 2 }}>
+            <Link href="/notes/[id]" as={`/notes/${note.id}`}>
               <a sx={{ textDecoration: "none", cursor: "pointer" }}>
                 <div sx={{ variant: "containers.card" }}>
                   <strong>{note.title}</strong>
@@ -36,5 +32,14 @@ const Notes = () => {
     </div>
   );
 };
+
+export async function getServerSideProps() {
+  const res = await fetch(`http://localhost:3000/api/note/`);
+  const { data } = await res.json();
+  console.log(data);
+  return {
+    props: { notes: data },
+  };
+}
 
 export default Notes;
