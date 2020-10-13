958fa215a969a18a821bf7d7fb7d6f3a4a308532
Vincent <Vincent.bouton.dev@gmail.com>
2020-10-13 20:16:37 +0200

    Init next-strapi project

---
+++ next-strapi/components/Header.jsx
@@ -0,0 +1,8 @@
+//DATE : 10/13/20
+
+import React from "react";
+
+const Header = () => {
+  return <header className={"Header"}>now i'm a real header</header>;
+};
+export default Header;
---
+++ next-strapi/pages/about.jsx
@@ -0,0 +1,8 @@
+//DATE : 10/13/20
+
+import React from "react";
+
+const About = () => {
+  return <div className={"about"}>i'm an about page</div>;
+};
+export default About;
